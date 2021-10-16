cb023cbf4a1f9f352e007abbccf4cf0f01013e5c
raylaslovich <ray.laslovich@gmail.com>
2021-10-15 21:10:02 -0600

    added customer name field

--- crud.js
+++ crud.js
@@ -15,6 +15,7 @@ function onFormSubmit(e) {
 //Retrieve the data
 function readFormData() {
     var formData = {};
+    formData["customerName"] = document.getElementById("customerName").value;
     formData["size"] = document.getElementById("size").value;
     formData["sauce"] = document.getElementById("sauce").value;
     formData["meatToppings"] = document.getElementById("meatToppings").value;
@@ -27,31 +28,35 @@ function insertNewRecord(data) {
     var table = document.getElementById("pizzaList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.customerName;
+    var cell1 = newRow.insertCell(1);
     cell1.innerHTML = data.size;
-    var cell2 = newRow.insertCell(1);
+    var cell2 = newRow.insertCell(2);
     cell2.innerHTML = data.sauce;
-    var cell3 = newRow.insertCell(2);
+    var cell3 = newRow.insertCell(3);
     cell3.innerHTML = data.meatToppings;
-    var cell4 = newRow.insertCell(3);
+    var cell4 = newRow.insertCell(4);
     cell4.innerHTML = data.vegToppings;
-    var cell5 = newRow.insertCell(4);
+    var cell5 = newRow.insertCell(5);
     cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
 }
 
 //Edit the data
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById('size').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('sauce').value = selectedRow.cells[1].innerHTML;
-    document.getElementById('meatToppings').value = selectedRow.cells[2].innerHTML;
-    document.getElementById('vegToppings').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('customerName').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('size').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('sauce').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('meatToppings').value = selectedRow.cells[3].innerHTML;
+    document.getElementById('vegToppings').value = selectedRow.cells[4].innerHTML;
 }
 
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.size;
-    selectedRow.cells[1].innerHTML = formData.sauce;
-    selectedRow.cells[2].innerHTML = formData.meatToppings;
-    selectedRow.cells[3].innerHTML = formData.vegToppings;
+    selectedRow.cells[0].innerHTML = formData.customerName;
+    selectedRow.cells[1].innerHTML = formData.size;
+    selectedRow.cells[2].innerHTML = formData.sauce;
+    selectedRow.cells[3].innerHTML = formData.meatToppings;
+    selectedRow.cells[4].innerHTML = formData.vegToppings;
 }
 
 //Delete the data
@@ -65,6 +70,7 @@ function onDelete(td) {
 
 //Reset the data
 function resetForm() {
+    document.getElementById('customerName').value = '';
     document.getElementById('size').value = 'Please Select';
     document.getElementById('sauce').value = 'Please Select';
     document.getElementById('meatToppings').value = 'Please Select';
